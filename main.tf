2f152e5c0ceaf5eac7a602a705c38cf1887fda93
sai-devopsguru <48646010+sai-devopsguru@users.noreply.github.com>
2021-09-25 22:02:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ variable "AWS_REGION" {
   default = "us-east-1"
 }
 resource "aws_instance" "example" {
-   ami           = var.AMIS[var.AWS_REGION]
+   ami           = ami-087c17d1fe0178315
    instance_type = "t2.micro"
 }
